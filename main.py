e78d8a3afe899bbb7e87ebb0741be6b8e916253e
LudensCS <1785932641@qq.com>
2025-10-18 16:08:23 +0800

    multilayer NN

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import NN
 
 if __name__ == "__main__":
-    NN.train(epoch_size=10, batch_size=100, learning_rate=0.01)
+    NN.train(epoch_size=10000, batch_size=100, learning_rate=0.01)
+    # NN.check_gradient()
